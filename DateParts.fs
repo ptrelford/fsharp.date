ec5a16f15b815431204f88bffdc78975f2b870ab
Phillip Trelford <ptrelford@hotmail.com>
2015-12-02 06:14:04 +0000

    Richer XML docs for provided types

--- DateParts.fs
+++ DateParts.fs
@@ -4,9 +4,6 @@ open System
 open System.Globalization
 open ProviderImplementation.ProvidedTypes
 
-let typeDef name = 
-   ProvidedTypeDefinition(name, baseType=Some typeof<obj>, HideObjectMethods=true)
-
 let createDay year month day =
    let date = DateTime(year,month,day)
    let dayName = day.ToString("D2")
@@ -18,27 +15,54 @@ let createDay year month day =
 let createDaysInMonth year month =
    [for day in 1..DateTime.DaysInMonth(year,month) -> createDay year month day]
 
-let createDaysOfWeek year month =      
-   [for dayOfWeek = 0 to 6 do
+let createDaysOfWeek year month =
+   [for dayOfWeek = 0 to 6 do 
       let dayOfWeek = enum<DayOfWeek> dayOfWeek
       let dayOfWeekName = Enum.GetName(typeof<DayOfWeek>, dayOfWeek)
-      let dayType = typeDef dayOfWeekName
-      for day = 1 to DateTime.DaysInMonth(year,month) do
-         if DateTime(year,month,day).DayOfWeek = dayOfWeek then
-            dayType.AddMemberDelayed(fun () -> createDay year month day)
+      let days =
+        [for day = 1 to DateTime.DaysInMonth(year,month) do
+            if DateTime(year,month,day).DayOfWeek = dayOfWeek then yield day]
+      let doc () = dayOfWeekName + String.Join(" or ", days)
+      let dayType = ProvidedTypeDefinition(dayOfWeekName, baseType=Some typeof<DateTime*DayOfWeek>, HideObjectMethods=true)
+      let ctor = ProvidedConstructor([],InvokeCode=fun _ -> <@@ (DateTime(year,month,1),dayOfWeek) @@>)      
+      ctor.AddXmlDocDelayed(doc)
+      dayType.AddMember(ctor)
+      dayType.AddXmlDocDelayed(doc)
+      for day in days do      
+         dayType.AddMemberDelayed(fun () -> createDay year month day)
       yield dayType
    ]
 
+let toCalendar (year,month) =
+   let date = System.DateTime(year,month,1)
+   let mondayDelta = (int date.DayOfWeek + 6) % 7
+   let mondayDelta = if mondayDelta = 0 then 7 else mondayDelta
+   [date.ToString("MMMM yyyy");"Mo Tu We Th Fr Sa Su"]
+   @
+   [for week in 0..5 ->
+      [|for weekDay in 0..6->
+         let days = week*7 + weekDay - mondayDelta
+         let day = date.AddDays(float days).Day
+         sprintf "%02d" day
+      |] |> String.concat " "
+   ]
+   |> List.map (fun s -> "<para>"+s+"</para>") 
+   |> String.concat "\r\n"
+
 let createMonth year month monthName =
-   let monthType = typeDef monthName
-   monthType.AddXmlDocDelayed(fun () -> CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month))
+   let doc () = "<summary>"+toCalendar(year,month)+"</summary>"
+   let monthType = ProvidedTypeDefinition(monthName, baseType=Some typeof<DateTime>, HideObjectMethods=true)   
+   let ctor = ProvidedConstructor([],InvokeCode=fun _ -> <@@ DateTime(year,month,1) @@>)   
+   ctor.AddXmlDocDelayed(doc)
+   monthType.AddMember(ctor)
+   monthType.AddXmlDocDelayed(doc)
    monthType.AddMembersDelayed(fun () -> createDaysInMonth year month)
    monthType.AddMembersDelayed(fun () -> createDaysOfWeek year month)
    monthType
 
 let createMonths year =
    [for month = 1 to 12 do
-      let monthName = month.ToString("D2")              
+      let monthName = month.ToString("D2")
       yield createMonth year month monthName
       let monthName = CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(month)
       yield createMonth year month monthName]
@@ -47,10 +71,12 @@ let createYears epoch =
    let thisYear = DateTime.Now.Year
    [for year = epoch to (thisYear + 10) do
       let yearName = year.ToString("D4")
-      let yearType = typeDef yearName
-      yearType.AddXmlDocDelayed(fun () -> "Year " + year.ToString())
-      yearType.AddMembersDelayed(fun () -> createMonths year)                      
+      let doc () = "Year " + year.ToString()
+      let yearType = ProvidedTypeDefinition(yearName, baseType=Some typeof<DateTime>, HideObjectMethods=true)
+      let ctor = ProvidedConstructor([],InvokeCode=fun _ -> <@@ DateTime(year,1,1) @@>)
+      ctor.AddXmlDocDelayed(doc)
+      yearType.AddMember(ctor)
+      yearType.AddXmlDocDelayed(doc)
+      yearType.AddMembersDelayed(fun () -> createMonths year)
       yield yearType
    ]
-
-
--- TimeTypeProvider.fs
+++ TimeTypeProvider.fs
@@ -12,8 +12,8 @@ type TimeProvider (config:TypeProviderConfig) as this =
    
    let createSeconds hours minutes =
       [for seconds = 0 to 59 do
-         let secondsName = seconds.ToString("D2")        
-         let getter args = <@@ TimeSpan(hours,minutes,seconds) @@>         
+         let secondsName = seconds.ToString("D2")
+         let getter args = <@@ TimeSpan(hours,minutes,seconds) @@>
          let property = ProvidedProperty(secondsName, typeof<TimeSpan>, IsStatic=true, GetterCode=getter)
          property.AddXmlDocDelayed(fun () -> TimeSpan(hours,minutes,seconds).ToString())
          yield property
@@ -22,25 +22,33 @@ type TimeProvider (config:TypeProviderConfig) as this =
    let createMinutes hours =
       [for minutes = 0 to 59 do
          let minutesName = minutes.ToString("D2")
-         let minutesType = ProvidedTypeDefinition(minutesName, baseType=Some typeof<obj>, HideObjectMethods=true)
+         let doc () = sprintf "%d Minutes" minutes
+         let minutesType = ProvidedTypeDefinition(minutesName, baseType=Some typeof<TimeSpan>, HideObjectMethods=true)
+         let ctor = ProvidedConstructor([],InvokeCode=fun _ -> <@@ TimeSpan(hours,minutes,0) @@>)
+         ctor.AddXmlDocDelayed(doc)
+         minutesType.AddMember(ctor)
          minutesType.AddMembersDelayed(fun () -> createSeconds hours minutes)
-         minutesType.AddXmlDocDelayed(fun () -> sprintf "%d Minutes" minutes)
+         minutesType.AddXmlDocDelayed(doc)
          yield minutesType
       ]
 
    let createHours () =
       [for hours = 0 to 23 do
          let hoursName = hours.ToString("D2")
-         let hoursType = ProvidedTypeDefinition(hoursName, baseType=Some typeof<obj>, HideObjectMethods=true)
+         let doc () = sprintf "%d Hours" hours
+         let hoursType = ProvidedTypeDefinition(hoursName, baseType=Some typeof<TimeSpan>, HideObjectMethods=true)
+         let ctor = ProvidedConstructor([],InvokeCode=fun _ -> <@@ TimeSpan(hours,0,0) @@>)
+         ctor.AddXmlDocDelayed(doc)
+         hoursType.AddMember(ctor)
          hoursType.AddMembersDelayed(fun () -> createMinutes hours)
-         hoursType.AddXmlDocDelayed(fun () -> sprintf "%d Hours" hours)
+         hoursType.AddXmlDocDelayed(doc)
          yield hoursType
       ]
 
    let ns = "FSharp.Date"
    let asm = Assembly.GetExecutingAssembly()
 
-   let timeType = ProvidedTypeDefinition(asm, ns, "TimeProvider", Some typeof<obj>)   
+   let timeType = ProvidedTypeDefinition(asm, ns, "TimeProvider", Some typeof<obj>)
    do  timeType.AddMembersDelayed(fun () -> createHours ()) 
 
    do  this.AddNamespace(ns, [timeType])
